ee9589080dfa9beb324253ed61b62188f3867647
Bruce Alexander <balexander@arifleet.ca>
2020-12-16 11:20:59 -0500

    creating echo skill & adding it to be called from the composer

---
+++ Integrate-Composer-Dialog-Using-Skill/Integrate_Composer_Dialog_Using_Skill/Controllers/EchoBotController.cs
@@ -0,0 +1,32 @@
+﻿using Integrate_Composer_Dialog_Using_Skill.Bots;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integrate_Composer_Dialog_Using_Skill.Controllers
+{
+    [Route("api/echo/messages")]
+    [ApiController]
+    public class EchoBotController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly IBot _bot;
+
+        public EchoBotController(IBotFrameworkHttpAdapter httpAdapter)
+        {
+            _adapter = httpAdapter;
+            _bot = new EchoBot();
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public async Task PostAsync()
+        {
+            await _adapter.ProcessAsync(Request, Response, _bot);
+        }
+    }
+}
