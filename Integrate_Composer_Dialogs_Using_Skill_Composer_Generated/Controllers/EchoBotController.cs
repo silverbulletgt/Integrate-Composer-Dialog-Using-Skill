1a7889afe2c0a9d7d36ceb151383d3c5d8bba06e
Bruce Alexander <balexander@arifleet.ca>
2021-01-15 15:36:32 -0500

    adding echo bot to composer generated project

---
+++ Integrate_Composer_Dialogs_Using_Skill_Composer_Generated/Controllers/EchoBotController.cs
@@ -0,0 +1,32 @@
+﻿using Integrate_Composer_Dialogs_Using_Skill_Composer_Generated.Bots;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integrate_Composer_Dialogs_Using_Skill_Composer_Generated.Controllers
+{
+    [Route("api/echo/messages")]
+    [ApiController]
+    public class EchoBotController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly IBot _bot;
+
+        public EchoBotController(IBotFrameworkHttpAdapter httpAdapter)
+        {
+            _adapter = httpAdapter;
+            _bot = new EchoBot();
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public async Task PostAsync()
+        {
+            await _adapter.ProcessAsync(Request, Response, _bot);
+        }
+    }
+}
