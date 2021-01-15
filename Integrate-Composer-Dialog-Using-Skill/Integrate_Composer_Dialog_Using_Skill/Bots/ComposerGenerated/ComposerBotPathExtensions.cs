1911baae043ad4bac7011b3b76f47fd94bc8cfbb
Bruce Alexander <balexander@arifleet.ca>
2021-01-15 10:53:25 -0500

    adding code pieces which were generated in the composer using the generate runtime code.  Attempting to use these instead of var composerDialog = resourceExplorer.LoadType<AdaptiveDialog>(dialogResource); to load the composer dialog since this code is currently preventing an upgrade to 4.11 of the bot framework nuget packages

---
+++ Integrate-Composer-Dialog-Using-Skill/Integrate_Composer_Dialog_Using_Skill/Bots/ComposerGenerated/ComposerBotPathExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integrate_Composer_Dialog_Using_Skill.Bots.ComposerGenerated
+{
+    /// <summary>
+    /// This code is from Microsoft.BotFramework.Composer.Core ComposerBotPathExtensions.cs
+    /// This class is generated when using Composer Generate Runtime for bot
+    /// </summary>
+    public static class ComposerBotPathExtensions
+    {
+        public static IConfigurationBuilder UseBotPathConverter(this IConfigurationBuilder builder)
+        {
+            var settings = new Dictionary<string, string>();
+            settings["bot"] = "ComposerDialogs";
+            builder.AddInMemoryCollection(settings);
+            return builder;
+        }
+    }
+}
---
+++ Integrate-Composer-Dialog-Using-Skill/Integrate_Composer_Dialog_Using_Skill/Bots/ComposerGenerated/LuisConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integrate_Composer_Dialog_Using_Skill.Bots.ComposerGenerated
+{
+    /// <summary>
+    /// This code is from Microsoft.BotFramework.Composer.Core ComposerBotPathExtensions.cs
+    /// This class is generated when using Composer Generate Runtime for bot
+    /// </summary>
+    public class LuisConfig
+    {
+        public string Name { get; set; }
+
+        public string DefaultLanguage { get; set; }
+
+        public List<string> Models { get; set; }
+
+        public string AuthoringKey { get; set; }
+
+        public bool Dialogs { get; set; }
+
+        public string Environment { get; set; }
+
+        public bool Autodelete { get; set; }
+
+        public string AuthoringRegion { get; set; }
+
+        public string Folder { get; set; }
+
+        public bool Help { get; set; }
+
+        public bool Force { get; set; }
+
+        public string Config { get; set; }
+
+        public string EndpointKeys { get; set; }
+    }
+}
